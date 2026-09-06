450403202b6f2f1a69481c254320a376968e540a
kbosnjak20 <karlobosnjak@gmail.com>
2026-09-06 16:47:40 +0200

    Initial commit

---
+++ AutoRent/Forms/MainForm.cs
@@ -0,0 +1,15 @@
+using System.Windows.Forms;
+
+namespace AutoRent.Forms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            Text = "AutoRent";
+            Width = 800;
+            Height = 500;
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+    }
+}
