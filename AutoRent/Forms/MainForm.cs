3b9a93b3f7b2002cf7ceb0f85548685f48180294
kbosnjak20 <karlobosnjak@gmail.com>
2026-09-07 03:26:03 +0200

    Dodan izbornik s navigacijom prema VozniPark

--- AutoRent/Forms/MainForm.cs
+++ AutoRent/Forms/MainForm.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows.Forms;
 
 namespace AutoRent.Forms
@@ -6,10 +7,36 @@ namespace AutoRent.Forms
     {
         public MainForm()
         {
-            Text = "AutoRent";
-            Width = 800;
-            Height = 500;
+            InitializeLayout();
+        }
+
+        private void InitializeLayout()
+        {
+            var employee = Session.CurrentEmployee;
+            Text = employee != null ? $"AutoRent - prijavljen/a: {employee} ({employee.Role})" : "AutoRent";
+            Width = 420;
+            Height = 380;
             StartPosition = FormStartPosition.CenterScreen;
+
+            var top = 20;
+            var btnVehicles = AddMenuButton("Vozni park", ref top);
+            btnVehicles.Click += (s, e) => new VehiclesForm().ShowDialog(this);
+
+            var btnLogout = new Button { Text = "Odjava", Left = 20, Top = 280, Width = 340, Height = 35 };
+            btnLogout.Click += (s, e) =>
+            {
+                Session.CurrentEmployee = null;
+                Application.Restart();
+            };
+            Controls.Add(btnLogout);
+        }
+
+        private Button AddMenuButton(string text, ref int top)
+        {
+            var button = new Button { Text = text, Left = 20, Top = top, Width = 340, Height = 40 };
+            Controls.Add(button);
+            top += 50;
+            return button;
         }
     }
 }
